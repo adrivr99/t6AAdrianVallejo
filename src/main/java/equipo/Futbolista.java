9491836d5fccea0aa0d12821da4207cc5feffcfe
adrivr99 <adri.vallejo.99@gmail.com>
2021-02-04 22:27:29 +0100

    Creacion Futbolista

---
+++ src/main/java/equipo/Futbolista.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package equipo;
+
+import java.time.LocalDate;
+import java.util.Objects;
+/**
+ *
+ * @author adriv
+ */
+public class Futbolista {
+    private String nombre;
+    private String apellidos;
+    private LocalDate fechaNacimiento;
+
+    public Futbolista() {
+        this.nombre = "Adrian";
+        this.apellidos = "Vallejo Robles";
+        this.fechaNacimiento = LocalDate.of(1999,8,15);
+    }
+
+    public Futbolista(String nombre, String apellidos, LocalDate fechaNacimiento) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.nombre);
+        hash = 71 * hash + Objects.hashCode(this.apellidos);
+        hash = 71 * hash + Objects.hashCode(this.fechaNacimiento);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Futbolista other = (Futbolista) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellidos, other.apellidos)) {
+            return false;
+        }
+        if (!Objects.equals(this.fechaNacimiento, other.fechaNacimiento)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Futbolista:" + "Nombre = " + nombre + ", Apellidos = " + apellidos + ", Fecha de nacimiento = " + fechaNacimiento;
+    }
+    
+    
+    
+    
+}
